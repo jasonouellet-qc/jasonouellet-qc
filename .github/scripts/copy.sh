23e8fea49acea35ca228e50ba638e36f9d014194
Jason Ouellet <69065603+jasonouellet-qc@users.noreply.github.com>
2024-01-24 15:19:10 +0000

    Ajout scripts pour workflow

---
+++ .github/scripts/copy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# bash boilerplate
+set -euo pipefail # strict mode
+readonly SCRIPT_NAME="$(basename "$0")"
+readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+function l { # Log a message to the terminal.
+    echo
+    echo -e "[$SCRIPT_NAME] ${1:-}"
+}
+
+# File to copy from personal repo
+README_FILE=$SRC_PATH/README.md
+
+# if the file exists in Notehub, copy it to Notehub-JS repo
+if [ -f "$README_FILE" ]; then
+    echo "Copying $README_FILE"
+    cp -R $README_FILE .
+fi
+
+echo "README file copied"
+
+
+# File to copy from personal repo
+IMAGES_PATH=$SRC_PATH/images
+IMAGES_PATTERN=$IMAGES_PATH/*badge*.*
+
+# if the file exists in Notehub, copy it to Notehub-JS repo
+if [ -f "$IMAGES_PATH" ]; then
+    echo "Copying $README_FILE"
+    cp -R $IMAGES_PATTERN ./images/.
+fi
+
+echo "images files copied"
---
+++ .github/scripts/push.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# bash boilerplate
+readonly SCRIPT_NAME="$(basename "$0")"
+readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+function l { # Log a message to the terminal.
+    echo
+    echo -e "[$SCRIPT_NAME] ${1:-}"
+}
+
+# check if there's already a currently existing feature branch in notehub-js for this branch
+# i.e. the altered openapi.yaml file's already been copied there at least once before
+#echo "Check if feature branch $BRANCH already exists in Notehub JS"
+#git ls-remote --exit-code --heads origin $BRANCH >/dev/null 2>&1
+#EXIT_CODE=$?
+EXIT_CODE=0
+#echo "EXIT CODE $EXIT_CODE"
+
+if [[ $EXIT_CODE == "0" ]]; then
+  #echo "Git branch '$BRANCH' exists in the remote repository"
+  # fetch branches from notehub-js
+  #git fetch
+  # stash currently copied openapi.yaml
+  #git stash
+  # check out existing branch from notehub-js
+  #git checkout $BRANCH 
+  # overwrite any previous openapi.yaml changes with current ones
+  #git checkout stash -- .
+else
+  #echo "Git branch '$BRANCH' does not exist in the remote repository"
+  # create a new branch in notehub-js 
+  #git checkout -b $BRANCH
+fi
+
+git add -A .
+git config user.name github-actions
+git config user.email github-actions@github.com
+git commit -am "feat: Update Personal profile"
+#git push --set-upstream origin $BRANCH
+
+echo "Updated personal profile successfully!"
